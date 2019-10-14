debf1b59f6f36e6e3c9532c0e1bfb61044798b1e
topeas <peiqixin@gmail.com>
2019-10-14 22:16:42 +0800

    feat:448找到所有数据中消失的数字

---
+++ 448.找到所有数组中消失的数字.js
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=448 lang=javascript
+ *
+ * [448] 找到所有数组中消失的数字
+ *
+ * https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array/description/
+ *
+ * algorithms
+ * Easy (53.16%)
+ * Likes:    210
+ * Dislikes: 0
+ * Total Accepted:    16.3K
+ * Total Submissions: 30.4K
+ * Testcase Example:  '[4,3,2,7,8,2,3,1]'
+ *
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ * 
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ * 
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入:
+ * [4,3,2,7,8,2,3, 1]
+ * 
+ * 输出:
+ * [5,6]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  let arr = []
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[Math.abs(nums[i]) - 1] > 0) {
+      nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1]
+    }
+  }
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] > 0) {
+      arr.push(j + 1)
+    }
+  }
+  return arr
+
+};
+// @lc code=end
+

3271759b47acb875d9800feb7b0fefe66d25ceef
topeas <peiqixin@gmail.com>
2019-10-29 20:53:54 +0800

    feat:三数之和

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -2,6 +2,33 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-    
+
+
+// 查看的答案
+var threeSum = function (_nums) {
+  let nums = _nums.sort((a,b) => a-b )
+  let len = nums.length
+  let res = []
+  if (!nums || nums.length < 3) return res
+  for (let i = 0; i < len - 2; i++) {
+    if (nums[i] > 0) return res
+    if (i > 0 && nums[i] === nums[i - 1]) continue
+    let L = i + 1
+    let R = len - 1
+    while (L < R) {
+      let sum = nums[i] + nums[L] + nums[R]
+      if (sum === 0) {
+        res.push([nums[i], nums[L], nums[R]]);
+        while (L < R && nums[L] == nums[L + 1]) L++;
+        while (L < R && nums[R] == nums[R - 1]) R--;
+        L++
+        R--
+      } else if (sum < 0) {
+        L++
+      } else if (sum > 0) {
+        R--
+      }
+    }
+  }
+  return res
 };

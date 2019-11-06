a7382039da927b103c9127fdd5b5eba3dc9064e6
topeas <peiqixin@gmail.com>
2019-11-06 10:28:33 +0800

    合并2个有序数组

--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -41,12 +41,25 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  let nums1 = nums1.concat(nums2)
-  for (let i = 0; i < m + n; i++) {
-    if (nums1[i] > nums1[m + i]) {
-        
+  let p = m + n - 1
+  let p1 = m - 1
+  let p2 = n - 1
+  while ((p1 >= 0) && (p2 >= 0)) {
+    if (nums2[p2] > nums1[p1]) {
+      nums1[p] = nums2[p2]
+      p2--
+      p--
+    } else {
+      nums1[p] = nums1[p1]
+      p1--
+      p--
     }
   }
+  while (p2 >= 0) {
+    nums1[p2] = nums2[p2]
+    p2--
+  }
 };
+// 这个地方是考虑过多了。
 // @lc code=end
 

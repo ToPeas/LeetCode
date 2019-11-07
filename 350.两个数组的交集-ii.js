0c1925863e0faf1f1c3ecdd8925e908b67066118
topeas <peiqixin@gmail.com>
2019-11-07 21:49:02 +0800

    feat:移动0

---
+++ 350.两个数组的交集-ii.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=javascript
+ *
+ * [350] 两个数组的交集 II
+ *
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (44.33%)
+ * Likes:    201
+ * Dislikes: 0
+ * Total Accepted:    53.1K
+ * Total Submissions: 118.2K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2,2]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [4,9]
+ * 
+ * 说明：
+ * 
+ * 
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现的次数一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 
+ * 
+ * 进阶:
+ * 
+ * 
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，磁盘内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+
+//  hash 表的形式
+var intersect = function (nums1, nums2) {
+  let map = {}
+  let ans = []
+  for (let i = 0; i < nums1.length; i++) {
+    if (!map[nums1[i]]) {
+      map[nums1[i]] = 1
+    } else {
+      map[nums1[i]]++
+    }
+  }
+  for (let j = 0; j < nums2.length; j++) {
+    if (map[nums2[j]]) {
+      ans.push(nums2[j])
+      map[nums2[j]]--
+    }
+  }
+  return ans
+};
+// @lc code=end
+
---
+++ 371.两整数之和.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=371 lang=javascript
+ *
+ * [371] 两整数之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-two-integers/description/
+ *
+ * algorithms
+ * Easy (51.83%)
+ * Likes:    161
+ * Dislikes: 0
+ * Total Accepted:    17.4K
+ * Total Submissions: 33.2K
+ * Testcase Example:  '1\n2'
+ *
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ * 
+ * 示例 1:
+ * 
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    
+};
+// @lc code=end
+

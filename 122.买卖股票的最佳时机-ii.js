ca2b2dbc8131ada194c821a5f594712c05bf1203
topeas <peiqixin@gmail.com>
2019-11-20 18:16:39 +0800

    update:打家劫舍

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -64,7 +64,7 @@ var maxProfit = function (prices) {
   let dp = []
   dp[0] = 0
   for (let i = 1; i < prices.length; i++) {
-    dp[i] = Math.max(dp[i - 1], dp[i - 1] + prices[i] - prices[i - 1])
+      dp[i] = Math.max(dp[i - 1], dp[i - 1] + prices[i] - prices[i - 1])
   }
   return dp[prices.length - 1]
 };

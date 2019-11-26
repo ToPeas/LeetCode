5ffa1251f5389bbc042265892aafd350c3d2f623
topeas <peiqixin@gmail.com>
2019-11-26 14:56:04 +0800

    update:3数之和

--- 16.最接近的三数之和.js
+++ 16.最接近的三数之和.js
@@ -29,8 +29,28 @@
  * @param {number} target
  * @return {number}
  */
-var threeSumClosest = function(nums, target) {
-    
+var threeSumClosest = function (nums, target) {
+  nums = nums.sort((a, b) => a - b)
+  console.log(nums)
+  let ans = nums[0] + nums[1] + nums[2]
+  for (let i = 0; i < nums.length; i++) {
+    let start = i + 1
+    let end = nums.length - 1
+    while (start < end) {
+      let sum = nums[start] + nums[i] + nums[end]
+      if (Math.abs(target - sum) < Math.abs(target - ans)) {
+        ans = sum
+      }
+      if (sum > target) {
+        end--
+      } else if (sum < target) {
+        start++
+      } else {
+        return ans
+      }
+    }
+  }
+  return ans
 };
 // @lc code=end
 

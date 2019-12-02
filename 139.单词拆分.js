19d146962969e0adeeff2240c9f519414cf46361
topeas <peiqixin@gmail.com>
2019-12-02 21:48:03 +0800

    update:单词拆分

---
+++ 139.单词拆分.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=139 lang=javascript
+ *
+ * [139] 单词拆分
+ *
+ * https://leetcode-cn.com/problems/word-break/description/
+ *
+ * algorithms
+ * Medium (42.68%)
+ * Likes:    243
+ * Dislikes: 0
+ * Total Accepted:    25.4K
+ * Total Submissions: 59.6K
+ * Testcase Example:  '"leetcode"\n["leet","code"]'
+ *
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * 
+ * 说明：
+ * 
+ * 
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+
+ // 动态规划 
+var wordBreak = function (s, wordDict) {
+  let n = s.length
+  let dp = Array(n + 1).fill(false)
+  dp[0] = true
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (dp[j] && wordDict.includes(s.substring(j, i))) {
+        dp[i] = true
+        break
+      }
+    }
+  }
+  return dp[n]
+};
+// @lc code=end
+

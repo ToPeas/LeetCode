4da0fc4080e4717803427bbc6a9c7f93eda9cea7
topeas <peiqixin@gmail.com>
2019-12-03 14:46:18 +0800

    update:排序链表

---
+++ 146.lru缓存机制.js
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=javascript
+ *
+ * [146] LRU缓存机制
+ *
+ * https://leetcode-cn.com/problems/lru-cache/description/
+ *
+ * algorithms
+ * Medium (43.59%)
+ * Likes:    310
+ * Dislikes: 0
+ * Total Accepted:    24.8K
+ * Total Submissions: 55.5K
+ * Testcase Example:  '["LRUCache","put","put","get","put","get","put","get","get","get"]\n' +
+  '[[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]]'
+ *
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+ * 
+ * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
+ * 写入数据 put(key, value) -
+ * 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
+ * 
+ * 进阶:
+ * 
+ * 你是否可以在 O(1) 时间复杂度内完成这两种操作？
+ * 
+ * 示例:
+ * 
+ * LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+ * 
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // 返回  1
+ * cache.put(3, 3);    // 该操作会使得密钥 2 作废
+ * cache.get(2);       // 返回 -1 (未找到)
+ * cache.put(4, 4);    // 该操作会使得密钥 1 作废
+ * cache.get(1);       // 返回 -1 (未找到)
+ * cache.get(3);       // 返回  3
+ * cache.get(4);       // 返回  4
+ * 
+ * 
+ * /
+
+// @lc code=start
+/**
+ * @param {number} capacity
+ */
+var LRUCache = class {
+
+  constructor(capacity) {
+    this.cache = new Map();
+    this.capacity = capacity;
+  }
+
+  /**
+   * @param {number} key
+   * @return {number}
+   */
+  get(key) {
+    let cache = this.cache;
+    if (cache.has(key)) {
+      let temp = cache.get(key)
+      cache.delete(key);
+      cache.set(key, temp);
+      return temp;
+    } else {
+      return -1;
+    }
+  };
+
+  /**
+   * @param {number} key
+   * @param {number} value
+   * @return {void}
+   */
+  put(key, value) {
+    let cache = this.cache;
+    if (cache.has(key)) {
+      cache.delete(key);
+    } else if (cache.size >= this.capacity) {
+      cache.delete(cache.keys().next().value);
+    }
+    cache.set(key, value);
+  };
+};
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// @lc code=end
+
--- 148.排序链表.js
+++ 148.排序链表.js
@@ -9,24 +9,51 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var sortList = function (head) {
-  if (!head) return null
-  let _head = head
-  function order(node) {
-    let nob = node
-    let left = head
-    let right = head
-    while (node) {
-      if (nob.val > node.val) {
-        left.next = node
-      } else {
-        right.next = node
-      }
-      node = node.next
-    }
-    order(left)
-    order(right)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var merge = function (l1, l2) {
+  if (!l1) return l2
+  if (!l2) return l1
+  if (l1.val <= l2.val) {
+      l1.next = merge(l1.next, l2)
+      return l1
+  } else {
+      l2.next = merge(l2.next, l1)
+      return l2
   }
-  order(head)
-  return _head
+}
+
+var helper = function (node) {
+  if (!node || !node.next) return node
+  let fast = node
+  let slow = node
+  let compare = node
+  while (fast && fast.next) {
+      fast = fast.next.next
+      compare = slow
+      slow = slow.next
+  }
+  compare.next = null
+  let l1 = helper(node)
+  let l2 = helper(slow)
+  return merge(l1, l2)
+}
+
+
+
+
+
+var sortList = function (head) {
+  return helper(head)
 };
+

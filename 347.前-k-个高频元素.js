b492a75e0d5492dc11a50104bb7e61cd2ea86100
topeas <peiqixin@gmail.com>
2019-12-09 15:32:43 +0800

    update:获取前K个高频

--- 347.前-k-个高频元素.js
+++ 347.前-k-个高频元素.js
@@ -41,8 +41,21 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
-    
+var topKFrequent = function (nums, k) {
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      map.set(nums[i], map.get(nums[i]) + 1)
+    } else {
+      map.set(nums[i], 1)
+    }
+  }
+  let res = []
+  let sortedArray = [...map.entries()].sort((a, b) => b[1] - a[1]);
+  for (let i = 0; i < k; i++) {
+    res.push(sortedArray[i][0]);
+  }
+  return res
 };
 // @lc code=end
 

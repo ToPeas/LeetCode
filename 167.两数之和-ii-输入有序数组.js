d7fdb6a0b67211b0b0511d6098b8589162027c6e
topeas <peiqixin@didiglobal.com>
2020-07-14 14:44:02 +0800

    update

---
+++ 167.两数之和-ii-输入有序数组.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=167 lang=javascript
+ *
+ * [167] 两数之和 II - 输入有序数组
+ *
+ * https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
+ *
+ * algorithms
+ * Easy (54.64%)
+ * Likes:    329
+ * Dislikes: 0
+ * Total Accepted:    107.2K
+ * Total Submissions: 196K
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ *
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ *
+ * 说明:
+ *
+ *
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ *
+ *
+ * 示例:
+ *
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+// 解法1，最low
+var twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    let cur = target - numbers[i]
+    let index = numbers.indexOf(cur, i + 1)
+    if (index > -1) {
+      return [i + 1, index + 1]
+    }
+  }
+}
+// 解法2，双指针
+var twoSum = function (numbers, target) {
+  let left = 0
+  let right = numbers.length - 1
+  while (left < right) {
+    let sum = numbers[left] + numbers[right]
+    if (sum === target) {
+      return [left + 1, right + 1]
+    }
+    if (sum > target) {
+      right--
+    }else {
+      left++
+    }
+  }
+}
+// @lc code=end

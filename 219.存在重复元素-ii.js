9826e81a86cec9869d4cad3a7da99ff8f695045c
topeas <peiqixin@didiglobal.com>
2020-08-03 17:20:01 +0800

    update

---
+++ 219.存在重复元素-ii.js
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=javascript
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (39.44%)
+ * Likes:    187
+ * Dislikes: 0
+ * Total Accepted:    54.2K
+ * Total Submissions: 135.9K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j 的差的
+ * 绝对值 至多为 k。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ *
+ * 示例 2:
+ *
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ *
+ * 示例 3:
+ *
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] in map)) {
+      map[nums[i]] = i
+    } else {
+      if (Math.abs(i - map[nums[i]]) <= k) {
+        return true
+      } else {
+        map[nums[i]] = i
+      }
+    }
+  }
+  return false
+}
+// @lc code=end
